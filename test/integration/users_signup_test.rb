c95328e3a7329bc78501f6d1f032752a703b200f
leejamesroberts <leejamesroberts@gmail.com>
2016-06-16 15:11:35 +0000

    Implementing basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,8 +19,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password_confirmation: "password" } }
     end
     follow_redirect!
-    # assert_template "user/show"
-    # assert_not flash.FILL_IN
+    assert_template "users/show"
+    assert is_logged_in?
   end
 end
 
