7601f554c93482fa8ca1d3c7a922122c1121cf99
leejamesroberts <leejamesroberts@gmail.com>
2016-07-18 19:22:21 +0000

    finish log in/logout/remember using cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,34 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user) # see note below
       redirect_to user
     else
-      flash.now[:danger] = "Invalid email/password combination" #not quite right as of yet
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
-  
 end
+
+# params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
+# is the same code as:
+
+# if params[:session][:remember_me]=='1'
+#   remember(user)
+# else
+#   forget(user)
+# end
+
+#the above example is a ternary operator!!!!
