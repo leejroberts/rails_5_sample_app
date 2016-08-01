12594b2e4889779a423613e21cbe851fcee38349
leejamesroberts <leejamesroberts@gmail.com>
2016-08-01 13:47:25 +0000

    modification to production might need to be rolled back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
-  config.action_mailer.perform_caching = false #might need to be commented out for sendgrid to work
+  # config.action_mailer.perform_caching = false #might need to be commented out for sendgrid to work
   config.action_mailer.raise_delivery_errors = true
   confing.action_mailer.delivery_method = :smtp
   host = aqueous-retreat-50960.herokuapp.com
