a28fb03a6808a7fe90a6b738c04baff631e595a3
leejamesroberts <leejamesroberts@gmail.com>
2016-08-19 13:06:00 +0000

    repair of aws access

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       # configuration for amazon s3
       :provider              => 'AWS',#AWS == amazon web services
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],#this variable is set in a heroku command
-      :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']#as is this one
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']#as is this one
     }
     config.fog_directory     = ENV['S3_BUCKET']#likely this one too.
   end
