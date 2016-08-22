306994a5c25c1fde3137dd1f392cdb08cf8ac418
leejamesroberts <leejamesroberts@gmail.com>
2016-08-22 12:43:36 +0000

    clean-up commit from last major commit

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # configuration for amazon s3
       :provider              => 'AWS',#AWS == amazon web services
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],#this variable is set in a heroku command
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']#as is this one
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'], #this variable is set in a heroku command
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']  #as is this one
     }
-    config.fog_directory     = ENV['S3_BUCKET']#likely this one too.
+    config.fog_directory     = ENV['S3_BUCKET'] # this one too. commands below
   end
 end
 #the function of this is to use AWS for image storage in the production environment with Heroku
@@ -14,6 +14,6 @@ end
 # the following are entered into the terminal for set-up purposes
   # the AWS user we set up is named lee...the access codes are somewhere safe
 
-# heroku config:set S3_ACCESS_KEY=<access key>
+# $ heroku config:set S3_ACCESS_KEY=<access key>
 # $ heroku config:set S3_SECRET_KEY=<secret key>
 # $ heroku config:set S3_BUCKET=<bucket name>
